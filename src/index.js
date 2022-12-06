a1c052053cc4095436961c281e0c2e27d0469437
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-12-06 23:03:03 +0400

    add menu, score block

--- src/index.js
+++ src/index.js
@@ -2,38 +2,108 @@ const gameField = document.querySelector('.gameField');
 const cellArray = document.querySelectorAll('.cell');
 const scoreBlock = document.querySelector('.scoreBlock');
 const span = document.createElement('span');
+const scoreLabel = document.createElement('label');
+const buttonStart = document.querySelector('.buttonStart');
+const buttonPause = document.querySelector('.buttonPause');
+const tbody = document.querySelector('tbody');
+
+const startCell = 16;
+const zeroCount = 0;
+const zeroCellCount = 0;
+const firstCellCount = 1;
+const fieldLength = 32;
+const firstX = 1;
+const firstY = 1;
+const firstCount = 1;
+const numberOfCells = 1024;
+const numberOfFruits = 3;
+const moveTime = 100;
+const timeInterval = 1000;
+const headSubtract1 = 1;
+const headSubtract2 = 2;
 
 let direction = "down";
 let score = 0;
 let interval;
+let startCount = 0;
+let isStart = false;
+let isReset = false;
+let isPause = false;
+let time = new Date(0).getMilliseconds();
 
 let snakeConfig = {
-    x: 16,
-    y: 16,
+    x: startCell,
+    y: startCell,
 }
 
 let snake = [];
 let fruits = [];
+let result = [];
 
 span.classList.add('score');
 span.innerText = score;
+scoreLabel.classList.add('scoreLabel');
+scoreLabel.innerText = 'score: ';
+scoreBlock.appendChild(scoreLabel);
 scoreBlock.appendChild(span);
 
+buttonStart.addEventListener('click', () => {
+    buttonStart.textContent = 'Reset';
+    isStart = !isStart;
+    isStart && (startCount === zeroCount) && startGame();
+    isReset && (startCount === firstCount) && restartGame();
+    isReset = true;
+    startCount = 1;
+});
+
+buttonPause.addEventListener('click', () => {
+    isPause = !isPause;
+});
+
+
+function setResult() {
+    const currentResult = {
+        score: score,
+        time: time,
+    }
+
+    window.localStorage.setItem('result', JSON.stringify(currentResult)); 
+}
 
 // Запустить игру
 
 function startGame() {
-    for (let i = 0; i < 1024; i++) {
+    for (let i = 0; i < numberOfCells; i++) {
         createCell();
         setCellAttribute();
     }
     
-    while(fruits.length < 3) {
+    while(fruits.length < numberOfFruits) {
         createFruit();
     }
 
+    
     createSnake();
-    interval  = setInterval(move, 100);
+    interval = setInterval(() => {
+        !isPause && move();
+    }, moveTime);
+
+    setInterval(() => {
+        time++;
+    }, timeInterval);
+
+    const results = JSON.parse(window.localStorage.getItem('result'));
+
+    if (results) {
+        const tr = document.createElement('tr');
+        const timeResult = document.createElement('td');
+        const scoreResult = document.createElement('td');
+        timeResult.textContent = results.time;
+        scoreResult.textContent = results.score;
+        tr.appendChild(timeResult);
+        tr.appendChild(scoreResult);
+        tbody.appendChild(tr);
+    }
 }
 
 
@@ -52,17 +122,17 @@ function createCell() {
 function setCellAttribute() {
     const cellArray = document.querySelectorAll('.cell');
 
-    let count = 0;
-    let x = 1;
-    let y = 1;
+    let count = zeroCellCount;
+    let x = firstX;
+    let y = firstY;
 
     for (let i = 0; i < cellArray.length; i++) {
         count++;
 
-        if (count > 32) {
+        if (count > fieldLength) {
             y++;
-            x = 1;
-            count = 1;
+            x = firstX;
+            count = firstCellCount;
         }
         cellArray[i].setAttribute('posX', x++);
         cellArray[i].setAttribute('posY', y);
@@ -74,8 +144,8 @@ function setCellAttribute() {
 
 function createSnake() {
     const headPosition = document.querySelector(`[posX="${snakeConfig.x}"][posY="${snakeConfig.y}"]`);
-    const bodyPosition = document.querySelector(`[posX="${snakeConfig.x}"][posY="${snakeConfig.y - 1}"]`);
-    const bodyPosition2 = document.querySelector(`[posX="${snakeConfig.x}"][posY="${snakeConfig.y - 2}"]`);
+    const bodyPosition = document.querySelector(`[posX="${snakeConfig.x}"][posY="${snakeConfig.y - headSubtract1}"]`);
+    const bodyPosition2 = document.querySelector(`[posX="${snakeConfig.x}"][posY="${snakeConfig.y - headSubtract2}"]`);
     snake.push(headPosition);
     snake.push(bodyPosition);
     snake.push(bodyPosition2);
@@ -105,10 +175,36 @@ function eatFruit(snakeCoordinates) {
             fruitPosition.classList.remove('fruit');
             fruits.splice(i, 1);
             createFruit();
+            setResult()
         }
     }
 }
 
+
+// Перезапуск игры
+
+function restartGame() {
+    if (isReset) {
+        for (let j = 0; j < snake.length; j++) {
+            snake[j].classList.remove("snakeBody");
+            snake[j].classList.remove("snakeHead");
+        }
+
+        for (let k = 0; k < fruits.length; k++) {
+            if (fruits[k].classList.contains("fruit")) {
+                fruits[k].classList.remove("fruit");
+            }
+        }
+        
+        snake = [];
+        fruits = [];
+        score = 0;
+        span.innerText = score;
+
+        clearInterval(interval);
+        startGame();
+    }
+}
 
 // Конец игры
 
@@ -117,9 +213,10 @@ function gameOver(snakeCoordinates) {
         if (snakeCoordinates[0] === snake[i].getAttribute('posX')
         && snakeCoordinates[1] === snake[i].getAttribute('posY')) {
             for (let j = 0; j < snake.length; j++) {
-                snake[j].classList.remove("snakeBody")
-                snake[j].classList.remove("snakeHead")
+                snake[j].classList.remove("snakeBody");
+                snake[j].classList.remove("snakeHead");
             }
+
             for (let k = 0; k < fruits.length; k++) {
                 if (fruits[k].classList.contains("fruit")) {
                     fruits[k].classList.remove("fruit");
@@ -151,7 +248,7 @@ function move() {
     gameOver(snakeCoordinates);
 
     if (direction === "down") {
-        if (snakeCoordinates[1] < 32) {
+        if (snakeCoordinates[1] < fieldLength) {
             snake.unshift(document.querySelector('[posX ="' + snakeCoordinates[0] + '"][posY ="' + (+snakeCoordinates[1] + 1) + '"]'))
         } else {
             snake.unshift(document.querySelector('[posX ="' + snakeCoordinates[0] + '"][posY ="1"]'))
@@ -175,7 +272,7 @@ function move() {
     }
 
     if (direction === "right") {
-        if (snakeCoordinates[0] < 32) {
+        if (snakeCoordinates[0] < fieldLength) {
             snake.unshift(document.querySelector('[posX ="' + (+snakeCoordinates[0] + 1) + '"][posY ="' + snakeCoordinates[1] + '"]'))
         } else {
             snake.unshift(document.querySelector('[posX ="1"][posY ="' + snakeCoordinates[1] + '"]'))
@@ -193,16 +290,16 @@ function move() {
 // Управление
 
 document.addEventListener("keydown", (event) => {
-    if (event.code === "KeyW" && direction !== "down") {
+    if (event.code === "KeyW" || event.code === "ArrowUp" && direction !== "down") {
         direction = 'up';
     }
-    if (event.code === "KeyS" && direction !== "up") {
+    if (event.code === "KeyS" || event.code === "ArrowDown"  && direction !== "up") {
         direction = 'down';
     }
-    if (event.code === "KeyA" && direction !== "right") {
+    if (event.code === "KeyA" || event.code === "ArrowLeft" && direction !== "right") {
         direction = 'left';
     }
-    if (event.code === "KeyD" && direction !== "left") {
+    if (event.code === "KeyD" || event.code === "ArrowRight" && direction !== "left") {
         direction = 'right';
     }
 })
@@ -220,12 +317,10 @@ function randomNumber(min, max) {
 // Создать фрукт
 
 function createFruit() {
-    const fruitPosX = randomNumber(1, 32);
-    const fruitPosY = randomNumber(1, 32);
+    const fruitPosX = randomNumber(firstCellCount, fieldLength);
+    const fruitPosY = randomNumber(firstCellCount, fieldLength);
     
     const fruitPosition = document.querySelector(`[posX="${fruitPosX}"][posY="${fruitPosY}"]`);
     fruitPosition.classList.add('fruit');
     fruits.push(fruitPosition);
 }
-
-startGame();
